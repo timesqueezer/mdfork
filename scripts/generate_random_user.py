cbe022492a0274cf574ed88d2f0aebdea27b1ea3
Timesqueezer <matzradloff@gmail.com>
2015-11-24 16:21:10 +0100

    Fixed scripts.

--- scripts/generate_random_user.py
+++ scripts/generate_random_user.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env/python
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import random
 from datetime import datetime, timedelta
 from mooddiary import create_app
