c6247926fd2fc40f2e08139b20eacf56cc0e3410
Timesqueezer <matzradloff@gmail.com>
2015-11-29 19:07:12 +0100

    OMG.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-sudo cp uwsgi_local.ini /etc/uwsgi/vassals/mooddiary.ini
+sudo cp uwsgi_config.ini /etc/uwsgi/vassals/mooddiary.ini
 sudo chown http:http /etc/uwsgi/vassals/mooddiary.ini
 sudo chmod 640 /etc/uwsgi/vassals/mooddiary.ini
