0bac8928b36c8fa6902fd57e8efb235e16ca3390
Timesqueezer <matzradloff@gmail.com>
2015-12-01 18:49:22 +0100

    Added user delete api stuff.

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,3 @@ git pull
 sudo cp uwsgi_config.ini /etc/uwsgi/vassals/mooddiary.ini
 sudo chown http:http /etc/uwsgi/vassals/mooddiary.ini
 sudo chmod 640 /etc/uwsgi/vassals/mooddiary.ini
-sudo tail -f /var/log/uwsgi/mooddiary.log
